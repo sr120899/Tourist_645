1672b641c2a74925e614a6e06c8878c6cfddb57d
sr120899 <ss.kedo.gg@gmail.com>
2024-05-13 16:38:46 +0700

    Add files via upload

---
+++ tourpoint.js
@@ -0,0 +1,155 @@
+var tourpoint = {
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Khao Yai National Park",
+          "type": "natural",
+          "level": 5
+        },
+        "geometry": {
+          "coordinates": [
+            101.3732988348317,
+            14.438437090285888
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "The Grand Palace",
+          "type": "curtural",
+          "level": 4
+        },
+        "geometry": {
+          "coordinates": [
+            100.49161594344184,
+            13.75096622319495
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Icon Siam",
+          "type": "spacial",
+          "level": 3
+        },
+        "geometry": {
+          "coordinates": [
+            100.51042899136513,
+            13.726669963061667
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Phu Kradueng National Park",
+          "type": "natural",
+          "level": 5
+        },
+        "geometry": {
+          "coordinates": [
+            101.76218785002385,
+            16.885415025444203
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Bangkok Art and Culture Centre (BACC)",
+          "type": "curtural",
+          "level": 4
+        },
+        "geometry": {
+          "coordinates": [
+            100.53034199136209,
+            13.746630421073505
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Benjakiti Park",
+          "type": "natural",
+          "level": 5
+        },
+        "geometry": {
+          "coordinates": [
+            100.5547439920361,
+            13.72959413914161
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Safari Park",
+          "type": "natural",
+          "level": 4
+        },
+        "geometry": {
+          "coordinates": [
+            99.50316995504988,
+            14.25299152592089
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Bang Pa-In Royal Palace",
+          "type": "curtural",
+          "level": 4
+        },
+        "geometry": {
+          "coordinates": [
+            100.57940390873824,
+            14.232360898362415
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Wat Yaichaimongkol",
+          "type": "curtural",
+          "level": 4
+        },
+        "geometry": {
+          "coordinates": [
+            100.59253981349013,
+            14.345787553770748
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Dream World",
+          "type": "spacial",
+          "level": 3
+        },
+        "geometry": {
+          "coordinates": [
+            100.67487099999909,
+            13.987485514359406
+          ],
+          "type": "Point"
+        }
+      }
+    ]
+  }
